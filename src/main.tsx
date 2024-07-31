e42f3a663352a70b9705f232971a5284c0d098a3
Jacob MG Evans <jacobmgevans@gmail.com>
2024-07-31 14:27:39 -0700

    Get Tailwind working

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/index.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 // Import the layout components
